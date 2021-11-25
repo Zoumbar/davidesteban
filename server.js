c4641082a3c18717ec6db55ae31ffba2f2cb7516
Cupico <davidelbar2@gmail.com>
2021-11-25 14:54:37 +0100

    ok

--- server.js
+++ server.js
@@ -22,54 +22,43 @@ const randomMovie = (req, res) => {
       `https://api.themoviedb.org/3/movie/${randomNumber}?api_key=0565c72220d110874a5b3f6fd2094dd7`
     )
     .then((response) => {
-      const data = response.data.title;
+      const title = response.data.title;
+      const note = response.data.vote_average;
+      const date = response.data.release_date;
+
       const result = {
         frames: [
           {
             index: 0,
-            text: data,
-            icon: "i5162",
+            text: title,
+            icon: "7862",
+          },
+          {
+            index: 1,
+            text: "Note : " + note + "/10",
+            icon: "626",
+          },
+          {
+            index: 2,
+            text: "Date : " + date,
+            icon: "626",
           },
         ],
       };
       res.json(result);
     })
     .catch((error) => {
-      const errorMovie = [
-        "Shrek",
-        "Spider-Man",
-        "Mortal Kombat",
-        "Inception",
-        "Interstellar",
-        "Jurassic Park",
-        "Star Wars : Attack of the clones",
-        "Rush Hour",
-        "The Incredible Hulk",
-        "Snowpiercer",
-        "21 Jump Street",
-        "Avatar",
-        "Scary Movie",
-        "Toy Story",
-        "Cars",
-        "Scream",
-        "Invisible-man",
-        "Easter Egg : david esteban les beaugoss",
-        "22 Jump Street",
-        "Cinderella",
-        "Karate Kid",
-      ];
-      const unfound = randomIntFromInterval(0, errorMovie.length);
       const result = {
         frames: [
           {
             index: 0,
-            text: errorMovie[unfound],
-            icon: "i5162",
+            text: "Wait few secondes...",
+            icon: "626",
           },
         ],
       };
       res.send(result);
-      console.log("eror lllol");
+
     });
 };
 
